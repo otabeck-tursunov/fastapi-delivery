23dbe4059fcddd809de88b09c8620eb09785caff
Otabek Tursunov <otabecktursunov@gmail.com>
2023-11-27 15:03:18 +0500

    project created

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {
+        'message': 'Asosiy sahifa'
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
