fd650fd834a695eb36e92073b5a7ea158cdf242a
Otabek Tursunov <otabecktursunov@gmail.com>
2023-11-27 17:16:44 +0500

    database.py added

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine('postgresql://postgres:123@localhost/delivery_db', echo=True)
+
+Base = declarative_base()
+Session = sessionmaker()
---
+++ order_routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+order_router = APIRouter(
+    prefix='/order'
+)
+
+@order_router.get('/')
+async def order():
+    return {
+        'message': 'Bu order route buyurtmalar sahifasi!'
+    }
